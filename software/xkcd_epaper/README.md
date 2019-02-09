c3822b20d51258701c05b45d02a42f391a8a875c
Holger Frey <mail@holgerfrey.de>
2019-02-09 16:59:24 +0100

    playing around with lookup tables

--- software/xkcd_epaper/README.md
+++ software/xkcd_epaper/README.md
@@ -6,6 +6,9 @@ Interface for the waveshare 4.2\" epaper display.
 
 This is derived from the [waveshare example code][wec], so all kudos to them.
 
+https://benkrasnow.blogspot.com/2017/10/fast-partial-refresh-on-42-e-paper.html
 
 
 [wec]: https://www.waveshare.com/wiki/File:4.2inch_e-paper_module_code.7z
+
+
--- software/xkcd_epaper/xkcd_epaper/__init__.py
+++ software/xkcd_epaper/xkcd_epaper/__init__.py
@@ -5,106 +5,12 @@ import subprocess
 
 from itertools import zip_longest
 
-# Pin definition
-RST_PIN = 17
-DC_PIN = 25
-CS_PIN = 8
-BUSY_PIN = 24
-
-# SPI device, bus = 0, device = 0
-SPI = spidev.SpiDev(0, 0)
-try:
-    result = subprocess.check_output(["cat", "/sys/module/spidev/parameters/bufsiz"])
-    SPI_BUFFER_SIZE = int(result.strip())
-except subprocess.CalledProcessError:
-    # small, but should do the trick
-    SPI_BUFFER_SIZE = 512
-
-# Display resolution
-EPD_WIDTH = 400
-EPD_HEIGHT = 300
-EPD_BUFFER_SIZE = EPD_WIDTH * EPD_HEIGHT // 8
-EPD_WHITE_IMAGE = [0xFF for i in range(0, EPD_BUFFER_SIZE)]
-EPD_BLACK_IMAGE = [0x00 for i in range(0, EPD_BUFFER_SIZE)]
-
-# EPD4IN2B commands
-PANEL_SETTING = 0x00
-POWER_SETTING = 0x01
-POWER_OFF = 0x02
-POWER_OFF_SEQUENCE_SETTING = 0x03
-POWER_ON = 0x04
-POWER_ON_MEASURE = 0x05
-BOOSTER_SOFT_START = 0x06
-DEEP_SLEEP = 0x07
-DATA_START_TRANSMISSION_1 = 0x10
-DATA_STOP = 0x11
-DISPLAY_REFRESH = 0x12
-DATA_START_TRANSMISSION_2 = 0x13
-VCOM_LUT = 0x20
-W2W_LUT = 0x21
-B2W_LUT = 0x22
-W2B_LUT = 0x23
-B2B_LUT = 0x24
-PLL_CONTROL = 0x30
-TEMPERATURE_SENSOR_CALIBRATION = 0x40
-TEMPERATURE_SENSOR_SELECTION = 0x41
-TEMPERATURE_SENSOR_WRITE = 0x42
-TEMPERATURE_SENSOR_READ = 0x43
-VCOM_AND_DATA_INTERVAL_SETTING = 0x50
-LOW_POWER_DETECTION = 0x51
-TCON_SETTING = 0x60
-RESOLUTION_SETTING = 0x61
-GSST_SETTING = 0x65
-GET_STATUS = 0x71
-AUTO_MEASURE_VCOM = 0x80
-VCOM_VALUE = 0x81
-VCM_DC_SETTING = 0x82
-PARTIAL_WINDOW = 0x90
-PARTIAL_IN = 0x91
-PARTIAL_OUT = 0x92
-PROGRAM_MODE = 0xA0
-ACTIVE_PROGRAM = 0xA1
-READ_OTP_DATA = 0xA2
-POWER_SAVING = 0xE3
-
-
-def delay_ms(delaytime):
-    """ small wrapper around time.sleep() """
-    time.sleep(delaytime / 1000.0)
-
-
-def grouper(iterable, n, fillvalue=None):
-    """ split an iterable in groups of n items
-
-    shamelessly copied from https://docs.python.org/3/library/itertools.html
-    """
-    args = [iter(iterable)] * n
-    return zip_longest(*args, fillvalue=fillvalue)
+from .config import *
+from .lut import Refresh
 
 
 class EPD:
     """ Interface for the Waveshare ePaper 4.2" display """
-
-    def send_command(self, command):
-        """ send a command to the display """
-        GPIO.output(DC_PIN, GPIO.LOW)
-        SPI.writebytes([command])
-
-    def send_data_byte(self, data):
-        """ sends one byte of data to the display """
-        GPIO.output(DC_PIN, GPIO.HIGH)
-        SPI.writebytes([data])
-
-    def send_data_list(self, data):
-        """ send lot of data to the display
-
-        the installed version of spidev doesn't have the writebytes2 function;
-        this is a pure python implementation. The buffer size is only half of
-        the size reported
-        """
-        GPIO.output(DC_PIN, GPIO.HIGH)
-        for buffer in grouper(data, SPI_BUFFER_SIZE):
-            SPI.writebytes([b for b in buffer if b is not None])
 
     def init(self):
         """ initialize the display """
@@ -128,10 +34,12 @@ class EPD:
         self.send_command(POWER_ON)
         self.wait_until_idle()
         self.send_command(PANEL_SETTING)
-        self.send_data_byte(0x0F)  # LUT from OTP
+        self.send_data_byte(0x3F)  # 300x400 B/W mode, LUT set by register
 
+        self.refresh = Refresh()
+
+        self._old_buffer = EPD_WHITE_IMAGE
         self._send_white_image(DATA_START_TRANSMISSION_1)
-
 
     def reset(self):
         """ hardware reset
@@ -144,6 +52,7 @@ class EPD:
 
     def clear(self):
         """ clear the display with a white image """
+        self.refresh.slow()
         self._send_white_image(DATA_START_TRANSMISSION_2)
         self.send_command(DISPLAY_REFRESH)
         self.wait_until_idle()
@@ -153,11 +62,20 @@ class EPD:
 
         the pixel values should amount to a length of 400 x 300 items
         """
+        t = time.time()
+        buffer = list(self._buffer_from_pixels(pixels))
+        print("creating buffer:", t-time.time())
+        t = time.time()
         self.send_command(DATA_START_TRANSMISSION_2)
-        buffer = self._buffer_from_pixels(pixels)
         self.send_data_list(buffer)
+        print("senging buffer:", t-time.time())
+        t = time.time()
         self.send_command(DISPLAY_REFRESH)
+        print("refresh:", t-time.time())
+        t = time.time()
         self.wait_until_idle()
+        print("wait idle:", t-time.time())
+
 
     def sleep(self):
         """ send the display into sleep """
@@ -184,4 +102,3 @@ class EPD:
         """ sends all white pixels using a transmission channel """
         self.send_command(transmission)
         self.send_data_list(EPD_WHITE_IMAGE)
-
